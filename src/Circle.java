e53886b8909bd0b163d7c65596cf94862aa65afc
Muskansainivaid <muskan01042002@gmail.com>
2022-01-24 14:56:17 -0500

    Test

--- src/Circle.java
+++ src/Circle.java
@@ -8,21 +8,7 @@
  * @author Muskan
  */
 public class Circle {
-    private double radius;
-
-    /**
-     * @return the radius
-     */
-    public double getRadius() {
-        return radius;
-    }
-
-    /**
-     * @param radius the radius to set
-     */
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
+    double radius;
     
 }
 
