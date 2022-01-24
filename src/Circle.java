af1a1b9d4bff5a0e2f984d9efb991f2c5a765494
Muskansainivaid <muskan01042002@gmail.com>
2022-01-24 13:03:10 -0500

    Added a class circle

---
+++ src/Circle.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Muskan
+ */
+public class Circle {
+    private double radius;
+
+    /**
+     * @return the radius
+     */
+    public double getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+}
+
