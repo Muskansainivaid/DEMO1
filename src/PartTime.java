4fcb293bbff980d08bb74ba306de45dc8c3746f6
Muskan <muskan01042002@gmail.com>
2022-01-31 11:42:09 -0500

    Add getter and setter

---
+++ src/PartTime.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Muskan
+ */
+public class PartTime {
+    private String Name;
+
+    /**
+     * @return the Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * @param Name the Name to set
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+}
